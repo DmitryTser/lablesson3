9bbccdb506d28d5c6d1a4ca987837131975a1797
DmitryTser <dimka.cer@gmail.com>
2019-12-20 17:06:29 +0300

    Initial commit

---
+++ intentapp/src/main/java/ru/mirea/tsertsenyuk/intentapp/second_activity.java
@@ -0,0 +1,26 @@
+package ru.mirea.tsertsenyuk.intentapp;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class second_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        TextView tv = findViewById(R.id.textView);
+        Intent intent = getIntent();
+        String text = intent.getStringExtra("message");
+        tv.setText(text);
+    }
+}
+
+
